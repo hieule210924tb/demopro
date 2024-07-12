5bf42b7b41880722f132d3e6576575c461af1491
hieu17 <leh364358@gmail.com>
2024-07-12 15:58:51 +0700

    100%

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-function writeLog(){
-    var myString ='';
-    for(var param of arguments){
-          myString += `${param} - `
+var slides  = document.querySelector(".show-sliders")
+var dots = document.querySelectorAll('.dot');
+var slideCount = document.querySelectorAll('.slide').length;
+let currentIndex = 0;
+function showSlide(index) {
+    if (index >= slideCount) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slideCount - 1;
+    } else {
+        currentIndex = index;
     }
-    console.log( myString)
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+    updateDots();
 }
-writeLog('log1' ,'log2','log3')
+function updateDots() {
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === currentIndex);
+    });
+}
+
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        const index = parseInt(dot.getAttribute('data-index'));
+        showSlide(index);
+    });
+});
+
+
